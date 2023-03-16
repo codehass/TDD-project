fae87b16069f6908ca92e72eeb0bc2beabb0c888
codehass <hassan.elouardy06@gmail.com>
2023-03-16 15:01:44 +0100

    Add testing for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,7 +8,9 @@ describe Solver do
       expect(solver).to be_instance_of Solver
     end
   end
-  describe '#factorial ' do
+
+  # testing for factorial method 
+  describe '#factorial' do
     it 'Return the factorial  of the number' do
       solver = Solver.new
 
@@ -26,7 +28,9 @@ describe Solver do
     end
   end
 
-  describe '#reverse ' do
+  #Testing for reverse method
+
+  describe '#reverse' do
     it 'Return the reverse of the word' do
       # arrange
       solver = Solver.new
@@ -34,6 +38,38 @@ describe Solver do
       word = solver.reverse('hello')
       # act
       expect(word).to be('olleh')
+    end
+  end
+
+  #Testing for fizzbuzz method
+
+  describe '#fizzbuzz' do
+    it 'Return the "fizz" when N divisible by 3' do
+      solver = Solver.new
+      number = solver.fizzbuzz(3)
+      fizzbuzz_number = "fizz"
+      expect(number).to eq(fizzbuzz_number)
+    end
+
+    it 'Return the "buzz" when N divisible by 5' do
+      solver = Solver.new
+      number = solver.fizzbuzz(5)
+      fizzbuzz_number = "buzz"
+      expect(number).to eq(fizzbuzz_number)
+    end
+
+    it 'Return the "fizzbuzz" when N divisible by 3 and 5'  do
+      solver = Solver.new
+      number = solver.fizzbuzz(15)
+      fizzbuzz_number = "fizzbuzz"
+      expect(number).to eq(fizzbuzz_number)
+    end
+
+    it 'Return the string "N" as string an any other case'  do
+      solver = Solver.new
+      number = solver.fizzbuzz(7)
+      fizzbuzz_number = "7"
+      expect(number).to eq(fizzbuzz_number)
     end
   end
 end
