a0f5fe9289fe38a0b2c760d26f30c218745f9913
codehass <hassan.elouardy06@gmail.com>
2023-03-16 14:40:22 +0100

    Add the test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require_relative '../solver'
+
+describe Solver do 
+  describe '#new' do
+    it 'Creates an instance of Solver' do
+      solver = Solver.new
+      expect(solver).to be_instance_of Solver
+    end
+    
+  describe '#factorial ' do 
+    it "Return the factorial  of the number" do
+      solver = Solver.new
+
+      #Negative number
+      negative_number = solver.factorial(-4)
+      expect(negative_number).to eq("Impassible to calculate the factorial")
+
+      #Number = 0
+      zero_number = solver.factorial(0)
+      expect(zero_number).to eq(1)
+
+      #Positive number
+      positive_number = solver.factorial(3)
+      expect(positive_number).to eq(6)
+    end
+  end
+
+
+end
