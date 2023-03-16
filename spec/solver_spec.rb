4ff28f6b431f6df3ed13cc772b113acf76b1d2e5
admirerbrown <admirerbrown@gmail.com>
2023-03-16 13:51:42 +0000

    create initial test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,30 +1,39 @@
 require 'rspec'
 require_relative '../solver'
 
-describe Solver do 
+describe Solver do
   describe '#new' do
     it 'Creates an instance of Solver' do
       solver = Solver.new
       expect(solver).to be_instance_of Solver
     end
-    
-  describe '#factorial ' do 
-    it "Return the factorial  of the number" do
+  end
+  describe '#factorial ' do
+    it 'Return the factorial  of the number' do
       solver = Solver.new
 
-      #Negative number
+      # Negative number
       negative_number = solver.factorial(-4)
-      expect(negative_number).to eq("Impassible to calculate the factorial")
+      expect(negative_number).to eq('Impassible to calculate the factorial')
 
-      #Number = 0
+      # Number = 0
       zero_number = solver.factorial(0)
       expect(zero_number).to eq(1)
 
-      #Positive number
+      # Positive number
       positive_number = solver.factorial(3)
       expect(positive_number).to eq(6)
     end
   end
 
-
+  describe '#reverse ' do
+    it 'Return the reverse of the word' do
+      # arrange
+      solver = Solver.new
+      # asset
+      word = solver.reverse('hello')
+      # act
+      expect(word).to be('olleh')
+    end
+  end
 end
